20750c577e319189e4c18b0521b01bac308b1b15
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2022-11-15 09:56:50 +0100

    Add CLR to populate database on application start

--- labb2/src/main/java/com/example/labb2/Labb2Application.java
+++ labb2/src/main/java/com/example/labb2/Labb2Application.java
@@ -1,7 +1,17 @@
 package com.example.labb2;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.geo.Point;
+
+import com.example.labb2.entity.Car;
+import com.example.labb2.entity.Parkingspot;
+import com.example.labb2.entity.Person;
+import com.example.labb2.repository.CarRepository;
+import com.example.labb2.repository.ParkingspotRepository;
+import com.example.labb2.repository.PersonRepository;
 
 @SpringBootApplication
 public class Labb2Application {
@@ -10,4 +20,18 @@ public class Labb2Application {
 		SpringApplication.run(Labb2Application.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(PersonRepository person, CarRepository car, ParkingspotRepository parking) {
+		return (args) -> {
+			var p1 = new Person("Bob", "Namnson");
+			person.save(p1);
+			var c1 = new Car("BMW", "Caar", p1);
+			car.save(c1);
+			Point point = new Point(1543.43, 1234.32);
+			var ps = new Parkingspot(point, 12.23);
+			parking.save(ps);
+		};
+	}
+
+	// Parkingspot p = new Parkingspot("fds", 12.34);
 }
