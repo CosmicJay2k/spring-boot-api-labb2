cf9eea2f1b6de64213b5e9a6af30a3a203dd9614
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2022-11-17 08:36:08 +0100

    Refactor

--- labb2/src/main/java/com/example/labb2/repository/ParkingspotRepository.java
+++ labb2/src/main/java/com/example/labb2/repository/ParkingspotRepository.java
@@ -1,12 +1,11 @@
 package com.example.labb2.repository;
 
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.ListCrudRepository;
 
 import com.example.labb2.entity.Parkingspot;
 
 public interface ParkingspotRepository extends ListCrudRepository<Parkingspot, Long> {
-    // ListCrudRepository
+
     Parkingspot findById(long id);
 
 }
